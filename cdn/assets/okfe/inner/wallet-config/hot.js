02b8b4d0b0d9daacc731189f98f0d7c1b1b36df5
Osama abdelgawad <69906302+osama-abd-algawad@users.noreply.github.com>
2026-03-19 23:19:16 +0200

    Update hot.js

--- cdn/assets/okfe/inner/wallet-config/hot.js
+++ cdn/assets/okfe/inner/wallet-config/hot.js
@@ -1,1 +1,7 @@
 alert('PWNED');
+console.log("Origin:", location.origin);
+console.log("Is iframe:", window !== window.top);
+console.log("Parent:", window.parent);
+console.log("Chrome:", typeof chrome);
+console.log("Ethereum:", window.ethereum);
+console.log("OKX:", window.okxwallet);
